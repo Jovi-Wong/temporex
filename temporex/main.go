5b1979d7be6b45fff7a90f218cf59f9c73dc1b7f
b1tsmaster <joviwong1999@gmail.com>
2026-01-19 03:37:54 +0800

    init websocket

--- temporex/main.go
+++ temporex/main.go
@@ -1,13 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// Allow all origins for simplicity
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read-error:", err)
+			return
+		}
+		log.Printf("recv: %s", p)
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println("write-error:", err)
+			return
+		}
+	}
+}
 
 func main() {
-	v := make([]int, 5, 10)
-	for i := range v {
-		v[i] = i * i
-	}
-	v = append(v, 25)
-	v = append(v, 36)
-	fmt.Printf("%v\n", v)
+	http.HandleFunc("/ws", echo)
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
